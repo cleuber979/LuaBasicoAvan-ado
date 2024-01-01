b473ca924115f837be5efefda2e68d900b737a10
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-01 04:38:59 -0300

    Add files via upload

---
+++ Estrutura de decisão lua.lua
@@ -0,0 +1,19 @@
+--Estruturas de decisão Lua
+local matouChefe = true
+if(matouChefe==true)then
+  print("Passou de fase")
+end
+
+--com else
+if(matouChefe==true)then
+  print("Passou de fase")
+else
+  print("Não passou de fase")
+end
+ 
+ local pontos = 8
+ if(matouChefe== true and pontos >=10)then
+   print("Passou de fase")
+ else
+   print("Não passou de fase")
+   end
